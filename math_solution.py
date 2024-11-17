c5bcf4ecee296986f0705f7339511776ee1135c1
ooooda <anya.smirdina@gmail.com>
2024-11-17 23:50:01 +0200

    Add files.

--- math_solution.py
+++ math_solution.py
@@ -1,6 +1,10 @@
 import numpy as np
 
 
+def get_projection(K, R, t):
+    return K @ np.hstack((R.T, -R.T @ t))
+
+
 def triangulation(
         camera_matrix: np.ndarray,
         camera_position1: np.ndarray,
@@ -21,5 +25,18 @@ def triangulation(
     :return: triangulated points, np.ndarray Nx3
     """
 
-    pass
-    # YOUR CODE HERE
+    points_3d = []
+    P1 = get_projection(camera_matrix, camera_rotation1, camera_position1)
+    P2 = get_projection(camera_matrix, camera_rotation2, camera_position2)
+    N = image_points1.shape[0]
+    for i in range(N):
+        u1, v1 = image_points1[i]
+        u2, v2 = image_points2[i]
+        A = np.zeros((4, 4))
+        A[0] = -u1 * P1[2] + P1[0]
+        A[1] = v1 * P1[2] - P1[1]
+        A[2] = -u2 * P2[2] + P2[0]
+        A[3] = v2 * P2[2] - P2[1]
+        _, _, Vt = np.linalg.svd(A)
+        points_3d.append(Vt[-1][:3] / Vt[-1][3])
+    return np.array(points_3d)
